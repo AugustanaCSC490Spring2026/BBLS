b02aff64a4e5c0b4e3dec68ff494bb2f0fe3c8d4
Lucas <lucasclayton0001@gmail.com>
2026-03-08 15:29:29 -0500

    Added some basic UI/functionality for the analytics page. Will need to edit it futher so it can handle single days and have more realistic data to pull from

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { useState } from "react";
+import { useNavigate } from "react-router-dom"; // React Router v6
+import Analytics from "./Analytics";
+
 
 function Dashboard() {
-    return (
-        <div>
-        <h1>Dashboard Page</h1>
-        </div>
-    )
-    }
+  const navigate = useNavigate();
+
+  const goToAnalytics = () => {
+    navigate("/analytics"); // route path to Analytics.jsx
+  };
+
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+      <button onClick={goToAnalytics}>Go to Analytics</button>
+    </div>
+  );
+}
+
 export default Dashboard;
