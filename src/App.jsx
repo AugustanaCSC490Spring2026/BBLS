8ec1c4cbb34e7fc7967038bc84fde4d79a2fbd83
benjamingeorgia <bengeorgia23@augustana.edu>
2026-03-09 21:06:17 -0500

    fixed file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Dashboard from "./Dashboard";
-import Analytics from "./Analytics";
+import Dashboard from "./pages/Dashboard";
+import Analytics from "./pages/Analytics";
 
 function App() {
   return (
