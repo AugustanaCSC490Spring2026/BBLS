8980a0ff8b9b1ea7b2c085d718ff910df6721023
benjamingeorgia <bengeorgia23@augustana.edu>
2026-05-19 16:49:33 -0500

    added more logic to the sendEmail file than just the transporter

--- functions/Email.js
+++ functions/Email.js
@@ -1,5 +1,6 @@
 import nodemailer from "nodemailer";
 import { defineSecret } from "firebase-functions/params";
+import { onCall, HttpsError } from "firebase-functions/v2/https";
 
 export const gmailUser = defineSecret("GMAIL_USER");
 export const gmailPass = defineSecret("GMAIL_APP_PASSWORD");
@@ -11,3 +12,28 @@ export const createTransporter = () => nodemailer.createTransport({
     pass: gmailPass.value(),
   }
 });
+
+export const sendEmail = onCall(
+  { secrets: [gmailUser, gmailPass] },
+  async (request) => {
+    if (!request.auth) {
+      throw new HttpsError("unauthenticated", "Must be logged in.");
+    }
+
+    const { to, subject, html } = request.data;
+
+    const transporter = createTransporter();
+
+    try {
+      await transporter.sendMail({
+        from: `"Campus Recreation" <${gmailUser.value()}>`,
+        to,
+        subject,
+        html
+      });
+      return { success: true };
+    } catch (error) {
+      throw new HttpsError("internal", error.message);
+    }
+  }
+);
